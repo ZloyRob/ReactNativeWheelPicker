e6758929e1d88a07490f58cede44cfffd71ac845
Kondratenkov Vadim <vadimandsanek@yandex.ru>
2024-08-12 17:54:32 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,5 +1,8 @@
+// @ts-ignore
 import WheelPicker from './src/WheelPicker'
+// @ts-ignore
 import TimePicker from './src/TimePicker'
+// @ts-ignore
 import DatePicker from './src/DatePicker'
 import { Picker } from '@react-native-picker/picker'
 import RNDateTimePicker from '@react-native-community/datetimepicker';
